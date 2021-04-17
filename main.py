22484b8461189d5d14f6bfd1561f6ee9cc9650e0
doquan01 <55120808+doquan01@users.noreply.github.com>
2021-04-17 16:21:59 -0700

    Test mouth

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import numpy as np
 face = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 eye = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
 smile=cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_smile.xml')
+mouth_cascade = cv2.CascadeClassifier('./cascade_files/haarcascade_mcs_mouth.xml')
 #1 for windows, 0 for mac
 cap=cv2.VideoCapture(0)
 
@@ -15,15 +16,23 @@ def baseCamera():
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         faces = face.detectMultiScale(gray,1.3,5)
         for (x,y,w,h) in faces:
-            cv2.rectangle(img, (x,y+h),(x+w,y+h), (0,0,255),2)
+            cv2.rectangle(img, (x,y),(x+w,y+h), (0,0,255),2)
             roi_g=gray[y:y+h,x:x+w]
             roi_c=img[y:y+h, x:x+w]
-            eyes=eye.detectMultiScale(roi_g)
-            for (ex,ey,ew,eh) in eyes:
-                cv2.rectangle(roi_c, (ex,ey),(ex+ew,ey+eh),(0,22,0),2)
+            # eyes=eye.detectMultiScale(roi_g)
+            # for (ex,ey,ew,eh) in eyes:
+            #     cv2.rectangle(roi_c, (ex,ey),(ex+ew,ey+eh),(0,22,0),2)
             cv2.imshow('img',img)
             k = cv2.waitKey(30)&0xff
             if k==27:
+                break
+            mouth_rects = mouth_cascade.detectMultiScale(gray, 1.7, 11)
+            for (x,y,w,h) in mouth_rects:
+                y = int(y - 0.15*h)
+                cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 3)
+                break
+            c = cv2.waitKey(30)&0xff
+            if c == 27:
                 break
     cap.release()
     cv2.destroyAllWindows()
