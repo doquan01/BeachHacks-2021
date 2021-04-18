df2c83ce7b82bc79db7738d67c9fc47a2b1ddcdd
RamiIsk <48740841+RamiIsk@users.noreply.github.com>
2021-04-18 01:00:29 -0700

    Mouth updatew
    version with working mouth

--- main.py
+++ main.py
@@ -1,141 +1,55 @@
-# # This is a sample Python script.
-
-# # Press ⌃R to execute it or replace it with your code.
 import cv2
-import numpy as np
 
-# # This is a sample Python script.
+# Method to draw boundary around the detected feature
+def draw_boundary(img, classifier, scaleFactor, minNeighbors, color, text):
+    # Converting image to gray-scale
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # detecting features in gray-scale image, returns coordinates, width and height of features
+    features = classifier.detectMultiScale(gray_img, scaleFactor, minNeighbors)
+    coords = []
+    # drawing rectangle around the feature and labeling it
+    for (x, y, w, h) in features:
+        cv2.rectangle(img, (x,y), (x+w, y+h), color, 2)
+        cv2.putText(img, text, (x, y-4), cv2.FONT_HERSHEY_SIMPLEX, 0.8, color, 1, cv2.LINE_AA)
+        coords = [x, y, w, h]
+    return coords
 
-# # Press ⌃R to execute it or replace it with your code.
-import cv2
-import numpy as np
 
-# face = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-# eye = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
-# smile=cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_smile.xml')
-# mouth_cascade = cv2.CascadeClassifier('./cascade_files/haarcascade_mcs_mouth.xml')
-# #1 for windows, 0 for mac
-# cap=cv2.VideoCapture(0)
+# Method to detect the features
+def detect(img, faceCascade, eyeCascade,mouthCascade):
+    color = {"blue":(255,0,0), "red":(0,0,255), "green":(0,255,0), "white":(255,255,255)}
+    coords = draw_boundary(img, faceCascade, 1.1, 10, color['blue'], "Face")
+    # If feature is detected, the draw_boundary method will return the x,y coordinates and width and height of rectangle else the length of coords will be 0
+    if len(coords)==4:
+        # Updating region of interest by cropping image
+        roi_img = img[coords[1]:coords[1]+coords[3], coords[0]:coords[0]+coords[2]]
+        # Passing roi, classifier, scaling factor, Minimum neighbours, color, label text
+        coords = draw_boundary(roi_img, eyeCascade, 1.1, 12, color['red'], "Eye")
+        # coords = draw_boundary(roi_img, noseCascade, 1.1, 4, color['green'], "Nose")
+        coords = draw_boundary(roi_img, mouthCascade, 1.1, 20, color['white'], "Mouth")
+    return img
 
-# def baseCamera():
-#     while True:
-#         ret, img = cap.read()
-#         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-#         faces = face.detectMultiScale(gray,1.3,5)
-#         for (x,y,w,h) in faces:
-#             cv2.rectangle(img, (x,y),(x+w,y+h), (0,0,255),2)
-#             roi_g=gray[y:y+h,x:x+w]
-#             roi_c=img[y:y+h, x:x+w]
-#             # eyes=eye.detectMultiScale(roi_g)
-#             # for (ex,ey,ew,eh) in eyes:
-#             #     cv2.rectangle(roi_c, (ex,ey),(ex+ew,ey+eh),(0,22,0),2)
-#             cv2.imshow('img',img)
-#             k = cv2.waitKey(30)&0xff
-#             if k==27:
-#                 break
-#             mouth_rects = mouth_cascade.detectMultiScale(gray, 1.7, 11)
-#             for (x,y,w,h) in mouth_rects:
-#                 y = int(y - 0.15*h)
-#                 cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 3)
-#                 break
-#             c = cv2.waitKey(30)&0xff
-#             if c == 27:
-#                 break
-#     cap.release()
-#     cv2.destroyAllWindows()
-# baseCamera()
 
-# # See PyCharm help at https://www.jetbrains.com/help/pycharm/
-cap = cv2.VideoCapture(0)
+# Loading classifiers
+faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades +'haarcascade_frontalface_default.xml')
+eyesCascade = cv2.CascadeClassifier(cv2.data.haarcascades +'haarcascade_eye.xml')
+# noseCascade = cv2.CascadeClassifier(cv2.data.haarcascades +'Nariz.xml')
+mouthCascade = cv2.CascadeClassifier('Mouth.xml')
 
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
-smile_cascade=cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_smile.xml')
-mouth_cascade = cv2.CascadeClassifier(cv2.data.haarcascades+'Mouth.xml')
+# Capturing real time video stream. 0 for built-in web-cams, 0 or -1 for external web-cams
+video_capture = cv2.VideoCapture(0)
 
-while (True):
-    ret, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        roi_gray = gray[y:y + h, x:x + w]
-        roi_color = frame[y:y + h, x:x + w]
-
-        eyes = eye_cascade.detectMultiScale(roi_gray)
-        mouth = mouth_cascade.detectMultiScale(roi_gray)
-        for (mx, my, mw, mh) in mouth:
-            cv2.rectangle(roi_color, (mx, my), (mx + mw, my + mh), (0, 0, 255), 2)
-
-            for (ex, ey, ew, eh) in eyes:
-                cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 0, 255), 2)
-
-    cv2.imshow('Frame', frame)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+while True:
+    # Reading image from video stream
+    _, img = video_capture.read()
+    # Call method we defined above
+    img = detect(img, faceCascade, eyesCascade, mouthCascade)
+    # Writing processed image in a new window
+    cv2.imshow("face detection", img)
+    if cv2.waitKey(30) & 0xFF == ord('q'):
         break
 
-    cap.release()
-    cv2.destroyAllWindows()
-# mouth_cascade = cv2.CascadeClassifier('./cascade_files/haarcascade_mcs_mouth.xml')
-# #1 for windows, 0 for mac
-# cap=cv2.VideoCapture(0)
-
-# def baseCamera():
-#     while True:
-#         ret, img = cap.read()
-#         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-#         faces = face.detectMultiScale(gray,1.3,5)
-#         for (x,y,w,h) in faces:
-#             cv2.rectangle(img, (x,y),(x+w,y+h), (0,0,255),2)
-#             roi_g=gray[y:y+h,x:x+w]
-#             roi_c=img[y:y+h, x:x+w]
-#             # eyes=eye.detectMultiScale(roi_g)
-#             # for (ex,ey,ew,eh) in eyes:
-#             #     cv2.rectangle(roi_c, (ex,ey),(ex+ew,ey+eh),(0,22,0),2)
-#             cv2.imshow('img',img)
-#             k = cv2.waitKey(30)&0xff
-#             if k==27:
-#                 break
-#             mouth_rects = mouth_cascade.detectMultiScale(gray, 1.7, 11)
-#             for (x,y,w,h) in mouth_rects:
-#                 y = int(y - 0.15*h)
-#                 cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 3)
-#                 break
-#             c = cv2.waitKey(30)&0xff
-#             if c == 27:
-#                 break
-#     cap.release()
-#     cv2.destroyAllWindows()
-# baseCamera()
-
-# # See PyCharm help at https://www.jetbrains.com/help/pycharm/
-cap = cv2.VideoCapture(0)
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
-mouth_cascade = cv2.CascadeClassifier('Mouth.xml')
-
-while (True):
-    ret, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        roi_gray = gray[y:y + h, x:x + w]
-        roi_color = frame[y:y + h, x:x + w]
-
-        eyes = eye_cascade.detectMultiScale(roi_gray)
-        mouth = mouth_cascade.detectMultiScale(roi_gray)
-        for (mx, my, mw, mh) in mouth:
-            cv2.rectangle(roi_color, (mx, my), (mx + mw, my + mh), (0, 0, 255), 2)
-
-            for (ex, ey, ew, eh) in eyes:
-                cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 0, 255), 2)
-
-    cv2.imshow('Frame', frame)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-    cap.release()
-    cv2.destroyAllWindows()
+# releasing web-cam
+video_capture.release()
+# Destroying output window
+cv2.destroyAllWindows()
